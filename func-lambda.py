11e3b7dd194d4922bebe842a37d71b8aa13c8f7b
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-23 10:52:37 +0800

    Create func-lambda.py

---
+++ func-lambda.py
@@ -0,0 +1,6 @@
+# Function and lambda
+def contoh_fungsi(sisi):
+    volume_kubus = sisi * sisi * sisi
+    print("volume kubus: ", volume_kubus)
+sisi = int(input("masukan nilai sisi: "))
+contoh_fungsi(sisi)
